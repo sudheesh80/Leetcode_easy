ba0c3e4a720dcc4a2bfbe573d97c22fc83c73da5
sudheesh80 <108114045+sudheesh80@users.noreply.github.com>
2022-06-29 09:06:17 -0400

    Create longest_commonPrefix.py

---
+++ longest_commonPrefix.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        strs1 = ["flower","flow","flight"]
+        strs = ["dog","racecar","car"]
+        short_strs = min(strs,key=len)
+        i=0
+        while short_strs and i < len(strs):
+            if not strs[i].startswith(short_strs):
+                short_strs = short_strs[:-1]
+            else:
+                i += 1
+        return short_strs
+tan = Solution()
+run = tan.longestCommonPrefix(str)
+print(run)

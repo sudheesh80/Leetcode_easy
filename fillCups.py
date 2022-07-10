9228cdaf34e3f466ca90adf5acd3e5f86f41ddad
sudheesh80 <108114045+sudheesh80@users.noreply.github.com>
2022-07-10 09:02:37 -0400

    Update fillCups.py

--- fillCups.py
+++ fillCups.py
@@ -1,17 +1,17 @@
-class Solution:
-    def fillCups(self, amount):
+class Solutions():
+    def fillCups(self,amount):
         count = 0
-        while amount[0] > 0 or amount[1] > 0 or amount[2] > 0:
-            amount.sort()
-            amount.reverse()
-            if amount[0] > 0:
+        while amount[0]>0 or amount[1]>0 or amount[2]>0:
+            amount.sort(reverse=True)
+            if amount[0]>0:
                 amount[0] -= 1
-            if amount[1] > 0:
-                amount[1] -= 1
-            elif amount[0] < 1 and amount[1] < 1:
-                amount[2] -= 1
+                if amount[1]>0:
+                    amount[1] -= 1
+                elif amount[2]>0:
+                    amount[2] -= 1
             count += 1
-        print(count)
-amount = [5,4,4]
-x= Solution()
-x.fillCups(amount)
+        return count
+
+amount = [5,0,0]
+x = Solutions()
+print(x.fillCups(amount))
